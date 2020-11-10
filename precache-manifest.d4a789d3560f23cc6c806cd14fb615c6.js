1bfb41d77cde3de1237ca4899df199bce02f10ac
Victor Rodriguez (Chile) Vendor <vn0tpgw@CL.wal-mart.com>
2020-11-10 13:14:11 -0300

    Updates

---
+++ precache-manifest.d4a789d3560f23cc6c806cd14fb615c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e2b8fd23d1d032fc58a27d99462871",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "fea461fd84b0a2fb4cac",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9fe05827f3e9cb43ec64",
+    "url": "/Robofriends/static/css/main.b85ee2d0.chunk.css"
+  },
+  {
+    "revision": "fea461fd84b0a2fb4cac",
+    "url": "/Robofriends/static/js/2.f8c53c40.chunk.js"
+  },
+  {
+    "revision": "cb4738a14a2ef86f47cb194746bb3116",
+    "url": "/Robofriends/static/js/2.f8c53c40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fe05827f3e9cb43ec64",
+    "url": "/Robofriends/static/js/main.3fea7d35.chunk.js"
+  },
+  {
+    "revision": "a6103a62c81f6271b765",
+    "url": "/Robofriends/static/js/runtime-main.79cc7977.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
